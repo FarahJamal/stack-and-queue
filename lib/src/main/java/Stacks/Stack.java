e5f31adc78cf2e0c65f56c4c81b34720d27d2188
FarahJamal <farahsarese@gmail.com>
2021-10-09 23:37:38 +0300

    stack anmd queue finished implementation

---
+++ lib/src/main/java/Stacks/Stack.java
@@ -0,0 +1,41 @@
+package Stacks;
+
+import Node.Node;
+
+public class Stack<T> {
+    private Node<T>top;
+
+    public Stack() {
+top=null;
+    }
+
+    public Node<T> getTop() {
+        return top;
+    }
+    public void push(T data){
+        Node<T> node=new Node<T>(data);
+        if(!isEmpty()){
+            node.setNext(top);
+            top=node;
+        }
+    }
+    public T pop() throws Exception {
+        T data;
+        if(isEmpty())
+            throw new Exception("sorry, empty stack!");
+        else{
+            data = top.getData();
+            top = top.getNext();
+        }
+        return data;
+    }
+    public T peek() throws Exception {
+        if(isEmpty())
+            throw new Exception("Can't peek empty Stack!");
+        else
+            return top.getData();
+    }
+    public boolean isEmpty(){
+        return top==null;
+    }
+}
